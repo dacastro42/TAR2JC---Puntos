efeb64bf41fea6525e68d0e0645c0b39f022c0f1
dacastro42 <108814849+dacastro42@users.noreply.github.com>
2025-02-08 09:54:07 -0500

    Initial commit
    Este es el inicio del proyecto

---
+++ src/modelo/Punto.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author JohanC
+ */
+public class Punto {
+    public double X;
+    public double Y;
+    
+
+    /**
+     * Get the value of Y
+     *
+     * @return the value of Y
+     */
+    public double getY() {
+        return Y;
+    }
+
+    /**
+     * Set the value of Y
+     *
+     * @param Y new value of Y
+     */
+    public void setY(double Y) {
+        this.Y = Y;
+    }
+
+    /**
+     * Get the value of X
+     *
+     * @return the value of X
+     */
+    public double getX() {
+        return X;
+    }
+
+    /**
+     * Set the value of X
+     *
+     * @param X new value of X
+     */
+    public void setX(double X) {
+        this.X = X;
+    }
+
+    public Punto() {
+    }
+
+    public Punto(double X, double Y) {
+        this.X = X;
+        this.Y = Y;
+    }
+
+    @Override
+    public String toString() {
+        return "Punto: " + "( " + X + " , " + Y + " )" + '\n';
+    }
+    
+    
+}
